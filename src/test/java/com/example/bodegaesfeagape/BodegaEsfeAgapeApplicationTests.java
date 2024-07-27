8f4483884314a3154102e618dd32ddbb32ea0a98
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-07-26 18:28:45 -0600

    Inicizalizacion del proyecto

---
+++ src/test/java/com/example/bodegaesfeagape/BodegaEsfeAgapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bodegaesfeagape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodegaEsfeAgapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
