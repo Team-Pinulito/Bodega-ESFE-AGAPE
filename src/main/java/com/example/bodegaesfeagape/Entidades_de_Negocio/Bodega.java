375fbce9a1e6c3c4fce3c7f24e5660b92fe9b5d6
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-08-13 18:33:04 -0600

    Modelos bodega y estante

--- src/main/java/com/example/bodegaesfeagape/Entidades_de_Negocio/Bodega.java
+++ src/main/java/com/example/bodegaesfeagape/Entidades_de_Negocio/Bodega.java
@@ -1,5 +1,11 @@
 package com.example.bodegaesfeagape.Entidades_de_Negocio;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
 @Entity
 @Table(name = "bodegas")
 public class Bodega {
@@ -7,4 +13,46 @@ public class Bodega {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @NotBlank(message = "El nombre es requerido")
+    private String nombre;
+
+    @NotBlank(message = "La descripción es requerida")
+    private String descripcion;
+
+    @OneToMany(mappedBy = "bodega")
+    private Set<Estante> estantes = new HashSet<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Set<Estante> getEstantes() {
+        return estantes;
+    }
+
+    public void setEstantes(Set<Estante> estantes) {
+        this.estantes = estantes;
+    }
+
+    
 }
