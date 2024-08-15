d227ee9c335899d52dd3e97138853a41c645dc57
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2024-08-14 21:04:19 -0600

    Se crea repositorio Rol

---
+++ src/main/java/com/example/bodegaesfeagape/Acceso_a_Datos/repositorios/IRol.java
@@ -0,0 +1,7 @@
+package com.example.bodegaesfeagape.Acceso_a_Datos.repositorios;
+
+import com.example.bodegaesfeagape.Entidades_de_Negocio.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IRol extends JpaRepository<Rol, Integer> {
+}
