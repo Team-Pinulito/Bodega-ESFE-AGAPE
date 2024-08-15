fd04cc69df65ae672048fb5bf61602c239256555
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2024-08-14 20:55:16 -0600

    Se sube clase Rol

--- src/main/java/com/example/bodegaesfeagape/Entidades_de_Negocio/Rol.java
+++ src/main/java/com/example/bodegaesfeagape/Entidades_de_Negocio/Rol.java
@@ -1,4 +1,47 @@
 package com.example.bodegaesfeagape.Entidades_de_Negocio;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "rol")
 public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "El nombre es requerido")
+    @Size(max = 100, message = "El nombre no debe exceder 100 caracteres")
+    private String nombre;
+
+    @OneToMany(mappedBy = "rol", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Usuario> usuarios = new HashSet<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public @NotBlank(message = "El nombre es requerido") @Size(max = 100, message = "El nombre no debe exceder 100 caracteres") String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(@NotBlank(message = "El nombre es requerido") @Size(max = 100, message = "El nombre no debe exceder 100 caracteres") String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Set<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(Set<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
 }
