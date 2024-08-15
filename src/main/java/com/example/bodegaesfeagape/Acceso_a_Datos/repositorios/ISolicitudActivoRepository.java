909cc6eeb0b2b473e4879954c28cebface725868
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-08-14 21:01:39 -0600

    se agrego el repositorio de solicitud activo

---
+++ src/main/java/com/example/bodegaesfeagape/Acceso_a_Datos/repositorios/ISolicitudActivoRepository.java
@@ -0,0 +1,9 @@
+package com.example.bodegaesfeagape.Acceso_a_Datos.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.bodegaesfeagape.Entidades_de_Negocio.SolicitudActivo;
+
+public interface ISolicitudActivoRepository extends JpaRepository<SolicitudActivo, Integer>{
+
+}
