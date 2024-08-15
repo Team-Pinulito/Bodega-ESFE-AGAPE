024ef0892f5e545b0d148f3b914cfc26bc4eeedf
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-08-14 21:00:58 -0600

    se creo el repositorio para detalle paquete activo

---
+++ src/main/java/com/example/bodegaesfeagape/Acceso_a_Datos/repositorios/IDetallePaqueteActivo.java
@@ -0,0 +1,9 @@
+package com.example.bodegaesfeagape.Acceso_a_Datos.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.bodegaesfeagape.Entidades_de_Negocio.DetallePaqueteActivo;
+
+public interface IDetallePaqueteActivo extends JpaRepository<DetallePaqueteActivo, Integer>{
+
+}
