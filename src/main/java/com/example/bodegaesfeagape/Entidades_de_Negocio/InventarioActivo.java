76830250189bae6a47cb852b8c95fe2cc58fb15c
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-08-14 20:38:31 -0600

    Se agrego Inventario activo y solicitud activo

--- src/main/java/com/example/bodegaesfeagape/Entidades_de_Negocio/InventarioActivo.java
+++ src/main/java/com/example/bodegaesfeagape/Entidades_de_Negocio/InventarioActivo.java
@@ -1,5 +1,91 @@
 package com.example.bodegaesfeagape.Entidades_de_Negocio;
 
+import java.util.HashSet;
+import java.util.Set;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "inventarioActivos")
 public class InventarioActivo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "activo_id")
+    private Activo activo;
+
+    @ManyToOne
+    @JoinColumn(name = "estante_id")
+    private Estante estante;
+
+    @NotNull(message = "La existencia es requerida")
+    private Integer existencia;
+
+    @OneToMany(mappedBy = "inventarioActivo")
+    private Set<KardexActivo> kardexActivos = new HashSet<>();
+
+    @OneToMany(mappedBy = "inventarioActivo")
+    private Set<AjusteInventario> ajusteInventarios = new HashSet<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Activo getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Activo activo) {
+        this.activo = activo;
+    }
+
+    public Estante getEstante() {
+        return estante;
+    }
+
+    public void setEstante(Estante estante) {
+        this.estante = estante;
+    }
+
+    public Integer getExistencia() {
+        return existencia;
+    }
+
+    public void setExistencia(Integer existencia) {
+        this.existencia = existencia;
+    }
+
+    public Set<KardexActivo> getKardexActivos() {
+        return kardexActivos;
+    }
+
+    public void setKardexActivos(Set<KardexActivo> kardexActivos) {
+        this.kardexActivos = kardexActivos;
+    }
+
+    public Set<AjusteInventario> getAjusteInventarios() {
+        return ajusteInventarios;
+    }
+
+    public void setAjusteInventarios(Set<AjusteInventario> ajusteInventarios) {
+        this.ajusteInventarios = ajusteInventarios;
+    }
+
+    
+
 
 }
--- src/main/java/com/example/bodegaesfeagape/Entidades_de_Negocio/SolicitudActivo.java
+++ src/main/java/com/example/bodegaesfeagape/Entidades_de_Negocio/SolicitudActivo.java
@@ -1,5 +1,158 @@
 package com.example.bodegaesfeagape.Entidades_de_Negocio;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.HashSet;
+@Entity
+@Table(name = "solicitudActivos")
 public class SolicitudActivo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull(message = "La fecha es requerida")
+    private LocalDateTime fecha;
+
+    @NotNull(message = "La fecha de entrega es requerida")
+    private LocalDateTime fechaEntrega;
+
+    @NotNull(message = "La fecha de recepción es requerida")
+    private LocalDateTime fechaRecepcion;
+
+    @NotNull(message = "La fecha de entrega esperada es requerida")
+    private LocalDateTime fechaEntregaEsperada;
+
+    @NotNull(message = "La fecha de recepción esperada es requerida")
+    private LocalDateTime fechaRecepcionEsperada;
+
+    @NotNull(message = "El comentario es requerido")
+    private String comentarios;
+
+    @NotBlank(message = "El correlativo es requerido")
+    private String correlativo;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "bodegueroEntrega_id")
+    private Usuario bodegueroEntrega;
+
+    @ManyToOne
+    @JoinColumn(name = "bodegueroRecibe_id")
+    private Usuario bodegueroRecibe;
+
+    @OneToMany(mappedBy = "solicitudActivo")
+    private Set<DetalleSolicitudActivo> detalleSolicitudActivos = new HashSet<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public LocalDateTime getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public void setFechaEntrega(LocalDateTime fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
+    }
+
+    public LocalDateTime getFechaRecepcion() {
+        return fechaRecepcion;
+    }
+
+    public void setFechaRecepcion(LocalDateTime fechaRecepcion) {
+        this.fechaRecepcion = fechaRecepcion;
+    }
+
+    public LocalDateTime getFechaEntregaEsperada() {
+        return fechaEntregaEsperada;
+    }
+
+    public void setFechaEntregaEsperada(LocalDateTime fechaEntregaEsperada) {
+        this.fechaEntregaEsperada = fechaEntregaEsperada;
+    }
+
+    public LocalDateTime getFechaRecepcionEsperada() {
+        return fechaRecepcionEsperada;
+    }
+
+    public void setFechaRecepcionEsperada(LocalDateTime fechaRecepcionEsperada) {
+        this.fechaRecepcionEsperada = fechaRecepcionEsperada;
+    }
+
+    public String getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    public String getCorrelativo() {
+        return correlativo;
+    }
+
+    public void setCorrelativo(String correlativo) {
+        this.correlativo = correlativo;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Usuario getBodegueroEntrega() {
+        return bodegueroEntrega;
+    }
+
+    public void setBodegueroEntrega(Usuario bodegueroEntrega) {
+        this.bodegueroEntrega = bodegueroEntrega;
+    }
+
+    public Usuario getBodegueroRecibe() {
+        return bodegueroRecibe;
+    }
+
+    public void setBodegueroRecibe(Usuario bodegueroRecibe) {
+        this.bodegueroRecibe = bodegueroRecibe;
+    }
+
+    public Set<DetalleSolicitudActivo> getDetalleSolicitudActivos() {
+        return detalleSolicitudActivos;
+    }
+
+    public void setDetalleSolicitudActivos(Set<DetalleSolicitudActivo> detalleSolicitudActivos) {
+        this.detalleSolicitudActivos = detalleSolicitudActivos;
+    }
+
+    
 
 }
