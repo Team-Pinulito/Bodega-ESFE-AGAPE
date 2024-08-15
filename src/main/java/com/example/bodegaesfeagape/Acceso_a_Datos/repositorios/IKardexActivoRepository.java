d407506cf1832f0fe48f4f760f40cb522114634a
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-08-14 20:15:58 -0600

    packge arreglos

--- src/main/java/com/example/bodegaesfeagape/Acceso_a_Datos/repositorios/IKardexActivoRepository.java
+++ src/main/java/com/example/bodegaesfeagape/Acceso_a_Datos/repositorios/IKardexActivoRepository.java
@@ -1,4 +1,4 @@
-package com.example.bodegaesfeagape.Acceso_a_Datos;
+package com.example.bodegaesfeagape.Acceso_a_Datos.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.bodegaesfeagape.Entidades_de_Negocio.KardexActivo;
