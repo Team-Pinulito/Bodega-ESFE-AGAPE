219f43b7b9ffab934634324b4bcbae2af5ca5061
Diego-Edenilson-Martinez-Lopez <7178497@clases.edu.sv>
2024-08-20 11:58:49 -0600

    se creo  la clase BL de Activo y el Controlador de Activo

---
+++ src/main/java/com/example/bodegaesfeagape/Logica_de_Negocio/clasesBL/ActivoBL.java
@@ -0,0 +1,37 @@
+package com.example.bodegaesfeagape.Logica_de_Negocio.clasesBL;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.example.bodegaesfeagape.Acceso_a_Datos.servicios.implementaciones.ActivoService;
+import com.example.bodegaesfeagape.Entidades_de_Negocio.Activo;
+
+public class ActivoBL {
+ @Autowired
+    private ActivoService activoService;
+
+    public Page<Activo> buscarTodosPaginados(Pageable pageable){
+        return activoService.buscarTodosPaginados(pageable);
+    }
+
+    public List<Activo> obtenerTodos() {
+        return activoService.obtenerTodos();
+    }
+
+    public Optional<Activo> buscarPorId(Integer id) {
+        return activoService.buscarPorId(id);
+    }
+
+    public Activo crearOEditar(Activo activo) {
+        return activoService.crearOEditar(activo);
+    }
+
+    public void eliminarPorId(Integer id) {
+        activoService.eliminarPorId(id);
+    }
+
+}
---
+++ src/main/java/com/example/bodegaesfeagape/Logica_de_Negocio/controladores/ActivoController.java
@@ -0,0 +1,68 @@
+package com.example.bodegaesfeagape.Logica_de_Negocio.controladores;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.bodegaesfeagape.Entidades_de_Negocio.Activo;
+import com.example.bodegaesfeagape.Logica_de_Negocio.clasesBL.ActivoBL;
+
+
+public class ActivoController {
+
+ @Autowired
+    private ActivoBL activoBL;
+
+    @GetMapping
+    public List<Activo> obtenerTodos() {
+        return activoBL.obtenerTodos();
+    }
+
+    @GetMapping("/paginados")
+    public Page<Activo> buscarTodosPaginados(Pageable pageable) {
+        return activoBL.buscarTodosPaginados(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Activo> buscarPorId(@PathVariable Integer id) {
+        Optional<Activo> activo = activoBL.buscarPorId(id);
+        return activo.map(ResponseEntity::ok)
+                     .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Activo> crearActivo(@RequestBody Activo activo) {
+        Activo nuevoActivo = activoBL.crearOEditar(activo);
+        return ResponseEntity.ok(nuevoActivo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Activo> editarActivo(@PathVariable Integer id, @RequestBody Activo activoDetalles) {
+        return activoBL.buscarPorId(id)
+                      .map(activo -> {
+                          activo.setNombre(activoDetalles.getNombre());
+                          activo.setDescripcion(activoDetalles.getDescripcion());
+                          activo.setEstante(activoDetalles.getEstante());
+                          activo.setTipoActivo(activoDetalles.getTipoActivo());
+                          activo.setCodigo(activoDetalles.getCodigo());
+                          activo.setCodigoDeBarra(activoDetalles.getCodigoDeBarra());
+                          Activo activoActualizado = activoBL.crearOEditar(activo);
+                          return ResponseEntity.ok(activoActualizado);
+                      }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarPorId(@PathVariable Integer id) {
+        return activoBL.buscarPorId(id)
+                      .map(activo -> {
+                          activoBL.eliminarPorId(id);
+                          return ResponseEntity.ok().<Void>build();
+                      }).orElse(ResponseEntity.notFound().build());
+    }
+}
