dbdcfda6702356fee07dba5cfbdf2e6d28d5f443
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-08-20 10:45:36 -0600

    se configuro rolbl como un servicio

--- src/main/java/com/example/bodegaesfeagape/Logica_de_Negocio/clasesBL/RolBL.java
+++ src/main/java/com/example/bodegaesfeagape/Logica_de_Negocio/clasesBL/RolBL.java
@@ -3,14 +3,18 @@ package com.example.bodegaesfeagape.Logica_de_Negocio.clasesBL;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import com.example.bodegaesfeagape.Acceso_a_Datos.servicios.implementaciones.RolService;
 import com.example.bodegaesfeagape.Entidades_de_Negocio.Rol;
 
+@Service
 public class RolBL {
 
+    @Autowired
     private RolService rolService;
 
     public Page<Rol> buscarTodosPaginados(Pageable pageable){
