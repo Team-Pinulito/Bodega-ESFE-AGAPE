ab5df6f1fad9117e4d8d057357163aadb6373ed3
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-08-24 10:54:23 -0600

    Se añadio la relacion hacia DetallePaqueteActivo en el modelo de PaqueteActivo

--- src/main/java/com/example/bodegaesfeagape/Acceso_a_Datos/servicios/implementaciones/PaqueteActivoService.java
+++ src/main/java/com/example/bodegaesfeagape/Acceso_a_Datos/servicios/implementaciones/PaqueteActivoService.java
@@ -5,11 +5,13 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import com.example.bodegaesfeagape.Acceso_a_Datos.repositorios.IPaqueteActivoRepository;
 import com.example.bodegaesfeagape.Acceso_a_Datos.servicios.interfaces.IPaqueteActivoService;
 import com.example.bodegaesfeagape.Entidades_de_Negocio.PaqueteActivo;
 
+@Service
 public class PaqueteActivoService implements IPaqueteActivoService {
 
     @Autowired
